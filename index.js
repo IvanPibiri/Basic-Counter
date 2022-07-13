f9fc3b517561fb78529797383158a3ea65baaefa
Ivan Pibiri <pibiriivan@gmail.com>
2022-07-13 17:27:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/* Set the count to 0 */
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+/* Calls a function for each button that set the class called by the callback function for every click on the respective button */
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList; console.log(e.currentTarget.classList)
+
+        /* Based on the class called, the count is updated */
+        if(styles.contains("decrease")) {
+            count--;
+        } else if(styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        /* I put some color to style number above or below zero */
+        if(count < 0) {
+            value.style.color = "orange";
+        } else if(count > 0) {
+            value.style.color = "lightblue";
+        } else {
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    })
+})
